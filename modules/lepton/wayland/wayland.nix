262d8ec0de567388f610f49859c196e77d6a7e0d
DeLoR1AnEc <cheatgameminecraft@gmail.com>
2026-04-01 23:09:09 +0200

    fixes

--- modules/lepton/wayland/wayland.nix
+++ modules/lepton/wayland/wayland.nix
@@ -9,20 +9,20 @@
           sessionVariables.NIXOS_OZONE_WL = "1";
         };
       };
-    };
 
     homeManager =
-    { user, ... }:
-    {
-      qt.enable = true;
-      gtk = {
-        enable = true;
-        gtk3.bookmarks = [
-          "file:///home/${user.userName}/Downloads Downloads"
-          "file:///home/${user.userName}/Documents Documents"
-          "file:///home/${user.userName}/Coding Coding"
-          "file:///home/${user.userName}/Art Art"
-        ];
+      { user, ... }:
+      {
+        qt.enable = true;
+        gtk = {
+          enable = true;
+          gtk3.bookmarks = [
+            "file:///home/${user.userName}/Downloads Downloads"
+            "file:///home/${user.userName}/Documents Documents"
+            "file:///home/${user.userName}/Coding Coding"
+            "file:///home/${user.userName}/Art Art"
+          ];
+        };
       };
-    };
+  };
 }
